74ab86713dfa5469b3967b45fe5def9a1c32731b
Renan Valladão <renancvalladao@gmail.com>
2022-11-21 16:34:31 -0300

    Initial implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
   Box,
   Button,
@@ -13,7 +13,7 @@ import Graph from "react-graph-vis";
 import GraphInput from "./GraphInput";
 
 type Node = {
-  id: number;
+  id: string;
   label: string;
 };
 
@@ -38,15 +38,48 @@ const options = {
   height: "500px",
 };
 
+const sortEdges = (edgesToBeSorted: Edge[]) => {
+  return edgesToBeSorted.sort((firstEdge: Edge, secondEdge: Edge): number => {
+    if (firstEdge.weight < secondEdge.weight) return -1;
+    if (firstEdge.weight > secondEdge.weight) return 1;
+    return 0;
+  });
+};
+
 function App() {
   const [nodes, setNodes] = useState<Node[]>([]);
   const [edges, setEdges] = useState<Edge[]>([]);
+  const [running, setRunning] = useState(false);
+  const [finished, setFinished] = useState(false);
+  const [visitedEdges, setVisitedEdges] = useState<Edge[]>([]);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (running) {
+      timer = setTimeout(() => {
+        if (counter < edges.length) {
+          const edge = edges[visitedEdges.length];
+          edge.color = "red";
+          if (visitedEdges.length < 2) {
+            setVisitedEdges((prevEdges) => [...prevEdges, edge]);
+          }
+          setCounter((prevCounter) => prevCounter + 1);
+        } else {
+          setRunning(false);
+          setFinished(true);
+        }
+      }, 1000);
+    }
+
+    return () => clearTimeout(timer);
+  }, [running, edges, visitedEdges, counter]);
 
   const readMatrix = (matrix: number[][]) => {
     const nodesArray: Node[] = [];
     const edgesArray: Edge[] = [];
     for (let i = 0; i < matrix.length; i++) {
-      nodesArray.push({ id: i, label: i.toString() });
+      nodesArray.push({ id: i.toString(), label: i.toString() });
       for (let j = 0; j < i + 1; j++) {
         const weight = matrix[i][j];
         if (weight > 0) {
@@ -104,21 +137,39 @@ function App() {
                     key={`item-${edge.from}-${edge.to}-${edge.label}`}
                   >
                     <ListItemText
+                      sx={{ color: edge.color }}
                       primary={`(${edge.from} - ${edge.to}) = ${edge.label}`}
                     />
                   </ListItem>
                 ))}
               </List>
             </Stack>
-            <Button
-              fullWidth
-              variant="contained"
-              color="success"
-              sx={{ marginTop: 2 }}
-              onClick={() => console.log("run")}
-            >
-              Run
-            </Button>
+            {!(running || finished) && (
+              <Button
+                fullWidth
+                variant="contained"
+                color="success"
+                sx={{ marginTop: 2 }}
+                onClick={() => {
+                  setRunning(true);
+                  const sortedEdges = sortEdges([...edges]);
+                  setEdges([...sortedEdges]);
+                }}
+              >
+                Run
+              </Button>
+            )}
+            {finished && (
+              <>
+                <Typography align="center" variant="h6" gutterBottom>
+                  Solution
+                </Typography>
+                <Graph
+                  graph={{ nodes, edges: visitedEdges }}
+                  options={options}
+                />
+              </>
+            )}
           </>
         )}
       </Box>
